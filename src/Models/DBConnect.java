868cc206f9112a347c0b1dfbefcc70d7d6866ce2
Dhvanit Merchant <200382214@student.georgianc.on.ca>
2018-09-14 09:55:01 -0400

    commit

---
+++ src/Models/DBConnect.java
@@ -0,0 +1,49 @@
+package Models;
+
+import java.sql.*;
+import java.util.ArrayList;
+
+public class DBConnect {
+    private static String user="student";
+    private static String password ="student";
+
+    public static ArrayList<String> getPhoneManufacturers() throws SQLException {
+        ArrayList<String> manufacturere = new ArrayList<>();
+        Connection conn = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+
+            try{
+                //1. Connect to database
+                conn =  DriverManager.getConnection("jdbc:mysql://localhost:3306/phones?useSSL=false",user,password);
+
+                //2. v=create a Statement
+                statement = conn.createStatement();
+                //3. create a Sql Query
+                resultSet = statement.executeQuery("Select * From manufacturers");
+                //4. loop over the results and add it to the ArrayList
+                while(resultSet.next())
+                {
+                    manufacturere.add(resultSet.getString("manufacturers"));
+
+                }
+
+            }
+            catch(SQLException e)
+            {
+                System.err.println(e);
+            }
+            finally {
+            if(conn != null)
+                conn.close();
+            if(statement != null)
+             statement.close();
+             if(resultSet!= null)
+             resultSet.close();
+            }
+
+        return manufacturere;
+    }
+
+
+}
---
+++ src/Models/DBTester.java
@@ -0,0 +1,9 @@
+package Models;
+
+public class DBTester {
+
+    public static void main(String[] args){
+        System.out.println(DBConnect.getPhoneManufacturers().)
+    }
+}
+
